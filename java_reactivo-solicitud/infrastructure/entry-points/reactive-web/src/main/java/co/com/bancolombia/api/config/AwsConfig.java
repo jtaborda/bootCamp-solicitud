9287886efb594e34ecf38c1725c209264d8a5efa
Jhonny Paul Taborda <jptaborda@unicauca.edu.co>
2025-09-10 21:24:02 -0500

    Update AwsConfig.java

--- java_reactivo-solicitud/infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/config/AwsConfig.java
+++ java_reactivo-solicitud/infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/config/AwsConfig.java
@@ -15,7 +15,7 @@ public class AwsConfig {
         return SqsAsyncClient.builder()
                 .region(Region.US_EAST_1)
                 .credentialsProvider(StaticCredentialsProvider.create(
-                        AwsBasicCredentials.create("AKIAUQMNCBVPZI3PFRPV", "N4PP/ikzwqNZDcMvUhCtVMsQkyERjDL9Gy5mpNwj")))
+                        
                 .build();
     }
 }
